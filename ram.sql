6311a653fddab0cd6212a0e3ac6c2a2eb707b676
satyakumarch <satyakumarchaudhary603@gmail.com>
2024-02-05 23:58:00 +0530

    first commit

---
+++ ram.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE colledge;
+USE  colledge;
+
+CREATE TABLE student (
+    rollno INT,
+    name VARCHAR(30),
+    age INT
+);
+INSERT INTO student VALUES
+(101,"Satya",12),(102,"kumar",13);
+
+SELECT * from student;
+
+creating the database of  student 
